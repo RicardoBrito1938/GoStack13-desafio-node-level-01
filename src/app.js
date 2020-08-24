a4dab0490bb609ac72e1898184b4df84ac5d2eb9
Ricardo TB <ricardo.jucrist@hotmail.com>
2020-08-24 09:08:14 -0300

    likes corrected

--- src/app.js
+++ src/app.js
@@ -75,17 +75,11 @@ app.delete("/repositories/:id", (request, response) => {
 app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params;
 
-  const repositoryIndex = repositories.findIndex(
-    repository => repository.id === id
-  );
+  const repository = repositories.find(repository => repository.id === id);
 
-  if (repositoryIndex < 0) {
-    return response.status(400).json({ error: "repository not found" });
-  }
+  repository.likes++;
 
-  repositories[repositoryIndex].likes++;
-
-  return response.json({ likes: repositories[repositoryIndex].likes });
+  return response.json(repository);
 });
 
 module.exports = app;
